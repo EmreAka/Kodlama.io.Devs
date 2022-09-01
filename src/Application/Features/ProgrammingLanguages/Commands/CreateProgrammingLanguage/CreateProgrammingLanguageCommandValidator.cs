cd4292f3129530a6dc51de6955d2743cd4133d56
Emre <aka548548@gmail.com>
2022-09-01 19:03:33 +0300

    Assignment 01/09/2022

---
+++ src/Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage;
+
+public class CreateProgrammingLanguageCommandValidator : AbstractValidator<CreateProgrammingLanguageCommand>
+{
+	public CreateProgrammingLanguageCommandValidator()
+	{
+		RuleFor(p => p.Name).NotEmpty().NotNull();
+	}
+}
