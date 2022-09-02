206b6fa93a128e81b64a64784e42dc058770541a
Emre <aka548548@gmail.com>
2022-09-02 09:30:26 +0300

    Assignment 02/09/2022

---
+++ src/Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguage/UpdateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage;
+
+public class UpdateProgrammingLanguageCommandValidator : AbstractValidator<UpdateProgrammingLanguageCommand>
+{
+	public UpdateProgrammingLanguageCommandValidator()
+	{
+		RuleFor(p => p.Name).NotEmpty().NotNull();
+		RuleFor(p => p.Id).NotNull().GreaterThanOrEqualTo(0);
+	}
+}
