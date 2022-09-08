c4352a3625eab55bae008a457f04078c88d00654
Emre <aka548548@gmail.com>
2022-09-08 16:07:58 +0300

    Entities for JWT/AUTH are configured.

--- src/Kodlama.io.Devs/Persistence/Contexts/BaseDbContext.cs
+++ src/Kodlama.io.Devs/Persistence/Contexts/BaseDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Core.Security.Entities;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -9,6 +10,11 @@ public class BaseDbContext : DbContext
     protected IConfiguration Configuration { get; set; }
     public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
     public DbSet<Technology> Technologies { get; set; }
+
+    //security
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    public DbSet<OperationClaim> OperationClaims { get; set; }
 
     public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
         => (Configuration) = (configuration);
@@ -37,6 +43,35 @@ public class BaseDbContext : DbContext
             p.Property(p => p.Name).HasColumnName("Name");
             p.Property(p => p.ProgrammingLanguageId).HasColumnName("ProgrammingLanguageId");
             p.HasOne(p => p.ProgrammingLanguage);
+        });
+
+        modelBuilder.Entity<User>(p =>
+        {
+            p.ToTable("Users").HasKey(k => k.Id);
+            p.Property(p => p.Id).HasColumnName("Id");
+            p.Property(p => p.FirstName).HasColumnName("FirstName");
+            p.Property(p => p.LastName).HasColumnName("LastName");
+            p.Property(p => p.Email).HasColumnName("Email");
+            p.Property(p => p.PasswordHash).HasColumnName("PasswordHash");
+            p.Property(p => p.PasswordSalt).HasColumnName("PasswordSalt");
+            p.Property(p => p.Status).HasColumnName("Status");
+            p.Property(p => p.AuthenticatorType).HasColumnName("AuthenticatorType");
+            p.HasMany(p => p.UserOperationClaims);
+            p.HasMany(p => p.RefreshTokens);
+        });
+
+        modelBuilder.Entity<OperationClaim>(p =>
+        {
+            p.ToTable("OperationClaims").HasKey(k => k.Id);
+            p.Property(p => p.Id).HasColumnName("Id");
+            p.Property(p => p.Name).HasColumnName("Name");
+        });
+
+        modelBuilder.Entity<UserOperationClaim>(p =>
+        {
+            p.ToTable("UserOperationClaims").HasKey(k => k.Id);
+            p.Property(p => p.Id).HasColumnName("Id");
+
         });
 
         ProgrammingLanguage[] programmingLanguagesSeeds = { new(1, "C#"), new(2, "Java"), new(3, "Dart") };
