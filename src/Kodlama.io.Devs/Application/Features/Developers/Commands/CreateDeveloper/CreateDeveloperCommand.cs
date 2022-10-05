b5666faecf18cb029971dfc7014d5398e209a046
Emre <aka548548@gmail.com>
2022-10-05 11:27:07 +0300

    Commented code is deleted.

--- src/Kodlama.io.Devs/Application/Features/Developers/Commands/CreateDeveloper/CreateDeveloperCommand.cs
+++ src/Kodlama.io.Devs/Application/Features/Developers/Commands/CreateDeveloper/CreateDeveloperCommand.cs
@@ -12,44 +12,6 @@ using Core.Security.Dtos;
 
 namespace Application.Features.Developers.Commands.CreateDeveloper;
 
-/*public class CreateDeveloperCommand : UserForRegisterDto, IRequest<TokenDto>
-{
-    public class CreateUserCommandHandler : IRequestHandler<CreateDeveloperCommand, TokenDto>
-    {
-        private readonly IDeveloperRepository _developerRepository;
-        private readonly IMapper _mapper;
-        private readonly ITokenHelper _tokenHelper;
-        private readonly DeveloperBusinessRules _developerBusinessRules;
-        private readonly IAuthService _authService;
-
-        public CreateUserCommandHandler(IMapper mapper, ITokenHelper tokenHelper,
-            IDeveloperRepository developerRepository, DeveloperBusinessRules developerBusinessRules,
-            IAuthService authService)
-            => (_mapper, _tokenHelper, _developerRepository, _developerBusinessRules, _authService) =
-                (mapper, tokenHelper, developerRepository, developerBusinessRules, authService);
-
-        public async Task<TokenDto> Handle(CreateDeveloperCommand request, CancellationToken cancellationToken)
-        {
-            await _developerBusinessRules.EmailCanNotBeDuplicatedWhenInserted(request.Email);
-
-            byte[] passwordHash, passwordSalt;
-            HashingHelper.CreatePasswordHash(request.Password, out passwordHash, out passwordSalt);
-
-            Developer developer = _mapper.Map<Developer>(request);
-            developer.PasswordHash = passwordHash;
-            developer.PasswordSalt = passwordSalt;
-
-            var createdDeveloper = await _developerRepository.AddAsync(developer);
-
-            var token = _tokenHelper.CreateToken(developer, new List<OperationClaim>());
-
-            var createdToken = _mapper.Map<TokenDto>(token);
-
-            return createdToken;
-        }
-    }
-}*/
-
 public class CreateDeveloperCommand : IRequest<RegisteredDto>
 {
     public UserForRegisterDto UserForRegisterDto { get; set; }
